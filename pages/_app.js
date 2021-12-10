c89caac8bc72e8ed3f357f6a0898ddcd9f0a2855
Kite_Z <duswl339@ajou.ac.kr>
2021-12-10 16:24:52 +0900

    SNS

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
+import { useHistory, useLocation} from "react-router-dom";
+
 
 function MyApp({ Component, pageProps }) {
+
   return <Component {...pageProps} />
 }
 
